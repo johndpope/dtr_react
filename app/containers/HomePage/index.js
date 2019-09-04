8b1dc364cae60db12c7e513ec658f9130596c597
Alvin John Tongco <christianpaul.torres@metrobank.com.ph>
2019-09-04 17:39:42 +0800

    Setup Boilerplate

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -2,17 +2,53 @@
  * HomePage
  *
  * This is the first thing users see of our App, at the '/' route
- *
  */
 
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import React, { useEffect, memo } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
-export default function HomePage() {
-  return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
-  );
+import { useInjectReducer } from 'utils/injectReducer';
+import { useInjectSaga } from 'utils/injectSaga';
+import reducer from './reducer';
+import saga from './saga';
+
+const key = 'home';
+
+class HomePage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    useInjectReducer({ key, reducer });
+    useInjectSaga({ key, saga });
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = createStructuredSelector({
+  
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(HomePage);
